d0a235f930cbef170c1dfc59ec19649a56875874
lastcaveman <caveman.last@gmail.com>
2019-01-20 15:24:51 +0800

    add requirements.txt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version = "0.1.7",
     keywords = ("pip", "weibo","微博","爬虫"),
     description = "weibo base class. 微博 爬虫 基础",
-    long_description = "Aweibo base class. 微博 爬虫 基础",
+    long_description = "weibo base class. 微博 爬虫 基础",
     license = "MIT Licence",
 
     url = "https://github.com/lastcaveman/weibo",     
